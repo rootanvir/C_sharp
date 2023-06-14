0576a878b0784092efcfd2721ac1eb706e68209f
rootanvir <103141061+rootanvir@users.noreply.github.com>
2023-06-14 22:08:46 +0600

    Update message_encryption.cs

--- message_encryption.cs
+++ message_encryption.cs
@@ -1,1 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleAppFirst
+{
+    internal class Program
+    {
+        public string encrypt(string str)
+        {
+            int[] key = { 2, -5, 5, 8, -4, 7 };
+            int j = 0;
+            string msg="";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (j == key.Length) { j = 0; }
+                char ch = (char)(str[i] + key[j]);
+                msg += Convert.ToString(ch);
+                j++;
+           
+            }
+            Console.WriteLine();
+            return msg;
+        }
+        public string decrpyt(string str)
+        {
+            int[] key = { 2, -5, 5, 8, -4, 7 };
+            int j = 0;
+            string msg = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (j == key.Length) { j = 0; }
+                char ch = (char)(str[i] - key[j]);
+                msg += Convert.ToString(ch);
+                j++;
+
+            }
+            Console.WriteLine();
+            return msg;
+
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string str = Console.ReadLine();
+            Console.WriteLine(p.decrpyt(str));            
+        }
+    }
+}
